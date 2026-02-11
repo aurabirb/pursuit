0b63ba42867a7022f36e43e8d597badac9056014
aurabirb <fed0000r@gmail.com>
2026-02-11 19:57:55 +0100

    Update copy script to download from upload server

--- copy-to-bazzite.sh
+++ copy-to-bazzite.sh
@@ -3,3 +3,5 @@
 rsync -arzhv0 ./barq_cache.db* user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/
 rsync --ignore-existing -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets
 
+# rsync -arzhv0 --info=progress2 ./validation.* furscan1:/home/ubuntu/furscan/
+rsync --ignore-existing -arzhv0 --info=progress2 furscan1:/home/ubuntu/furscan/pursuit_masks/ ./pursuit_masks/

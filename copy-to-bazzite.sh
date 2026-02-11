52a36da1de1d82663f213c6e1192942305887526
aurabirb <fed0000r@gmail.com>
2026-02-11 19:47:09 +0100

    Add systemd unit and copy scripts

--- copy-to-bazzite.sh
+++ copy-to-bazzite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -arzhv0 ./barq_images/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/barq_images
+# rsync -arzhv0 ./datasets/pursuit/barq/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/pursuit/barq/
 rsync -arzhv0 ./barq_cache.db* user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/
-rsync -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets
+rsync --ignore-existing -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets
 

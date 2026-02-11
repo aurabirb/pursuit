98084e2bbdbd1893f866542abd4b577b6e461bb5
aurabirb <fed0000r@gmail.com>
2026-02-11 21:13:01 +0100

    Fix copy script

--- copy-to-bazzite.sh
+++ copy-to-bazzite.sh
@@ -2,10 +2,10 @@
 # rsync -arzhv0  ./datasets/pursuit/barq/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/pursuit/barq/
 rsync -arzhv0 --info=progress2 ./barq_cache.db* user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/
 
-rsync --ignore-existing -arzhv0 user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets ./datasets/
+rsync --ignore-existing -arzhv0 user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/ ./datasets/
 rsync --ignore-existing -arzhv0 furscan1:/home/ubuntu/furscan/datasets/ ./datasets/
 rsync --ignore-existing -arzhv0 ./datasets/ furscan1:/home/ubuntu/furscan/datasets/
-rsync --ignore-existing -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets
+rsync --ignore-existing -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/
 
 # rsync -arzhv0 --info=progress2 ./validation.* furscan1:/home/ubuntu/furscan/
 rsync --ignore-existing -arzhv0 user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/pursuit_masks/ ./pursuit_masks/

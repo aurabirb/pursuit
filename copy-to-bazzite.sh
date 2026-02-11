bdb728bb38e7bcca0dde5b1c0aabdc77bb51d261
aurabirb <fed0000r@gmail.com>
2026-02-11 20:58:30 +0100

    Improved copy script

--- copy-to-bazzite.sh
+++ copy-to-bazzite.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# rsync -arzhv0 ./datasets/pursuit/barq/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/pursuit/barq/
-rsync -arzhv0 ./barq_cache.db* user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/
+# rsync -arzhv0  ./datasets/pursuit/barq/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets/pursuit/barq/
+rsync -arzhv0 --info=progress2 ./barq_cache.db* user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/
+
+rsync --ignore-existing -arzhv0 user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets ./datasets/
+rsync --ignore-existing -arzhv0 furscan1:/home/ubuntu/furscan/datasets/ ./datasets/
+rsync --ignore-existing -arzhv0 ./datasets/ furscan1:/home/ubuntu/furscan/datasets/
 rsync --ignore-existing -arzhv0 ./datasets/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/datasets
 
 # rsync -arzhv0 --info=progress2 ./validation.* furscan1:/home/ubuntu/furscan/
-rsync --ignore-existing -arzhv0 --info=progress2 furscan1:/home/ubuntu/furscan/pursuit_masks/ ./pursuit_masks/
+rsync --ignore-existing -arzhv0 user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/pursuit_masks/ ./pursuit_masks/
+rsync --ignore-existing -arzhv0 furscan1:/home/ubuntu/furscan/pursuit_masks/ ./pursuit_masks/
+rsync --ignore-existing -arzhv0 ./pursuit_masks/ user@bazzite:/media/user/SSD2TB/rutorch2/pursuit/pursuit_masks/
+rsync --ignore-existing -arzhv0 ./pursuit_masks/ furscan1:/home/ubuntu/furscan/pursuit_masks/
